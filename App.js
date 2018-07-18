37644199b4e5b2d332d88a11e83ff87cd15ae3ed
André Hoffmann <andre8hoffmann@gmail.com>
2018-07-18 18:43:05 -0300

    Primeiro iteração na criação do menu, com imagem, nome, e espaço para 2 botões.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux';
@@ -10,6 +10,8 @@ import rootReducer from './State';
 import GameInfo from './Components/GameInfo';
 import Board from './Components/Board/';
 
+import mouse from './mouseWhite.png'
+
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk)
@@ -19,13 +21,43 @@ export default class App extends Component {
   render(){
     return(
       <Provider store={store}>
-        <View style={{flex: 1}}>
-        	<View style={{flex: 2, backgroundColor: "#263238", borderBottomWidth: 10, borderColor: "#d50000"}}>
-       			<GameInfo />
-        	</View>
-        	<Board />
-        	<View style={{flex: 1, backgroundColor: "#263238", borderTopWidth: 10, borderColor: "#d50000"}}/>
-        </View>
+        <View style={{flex: 1,}}>
+          <View style={{
+            flex: 1, 
+            backgroundColor: "#263238", 
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+              <Image 
+                source={mouse} 
+                style={{width: 150, height: 150, marginBottom: 50}}
+              />
+              <Text style={{
+                fontSize: 80, 
+                color: "white",
+                fontFamily: "monospace",   
+                marginBottom: 50,
+              }}>
+                Timbiriche
+              </Text>
+      
+            <View style={{backgroundColor: "black", height: 100, width: 300, marginTop: 30}} />
+            <View style={{backgroundColor: "black", height: 100, width: 300, marginTop: 30}} />
+
+          </View>
+
+
+
+          {false&&
+            <View style={{flex: 1}}>
+            	<View style={{flex: 2, backgroundColor: "#263238", borderBottomWidth: 10, borderColor: "#d50000"}}>
+           			<GameInfo />
+            	</View>
+            	<Board />
+            	<View style={{flex: 1, backgroundColor: "#263238", borderTopWidth: 10, borderColor: "#d50000"}}/>
+            </View>
+          }
+        </View> 
       </Provider>
     )
   }

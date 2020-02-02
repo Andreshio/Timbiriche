a337206d756fa5346aab550b32f5964ea1db3ba8
andre.hoffmann(phi) <andre.hoffmann@meta.com.br>
2020-02-02 16:18:56 -0300

    retornando App.js para a versao correta

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component, Fragment} from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './State';
+
+import GameInfo from './Components/GameInfo';
+import Board from './Components/Board/';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+export default class App extends Component {
+  render(){
+    return(
+      <Provider store={store}>
+        <View style={{flex: 1}}>
+        	<View style={{flex: 2, backgroundColor: "#263238", borderBottomWidth: 10, borderColor: "#d50000"}}>
+       			<GameInfo />
+        	</View>
+        	<Board />
+        	<View style={{flex: 1, backgroundColor: "#263238", borderTopWidth: 10, borderColor: "#d50000"}}/>
+        </View>
+      </Provider>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
